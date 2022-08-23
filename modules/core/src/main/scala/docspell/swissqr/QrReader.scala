98287467f1eedcef88e5f7c85eba6d7ca520a145
eikek <eike.kettner@posteo.de>
2022-08-23 11:20:36 +0200

    Run first on original files and then on converted
    If no qr code is found try adding a small border to support images
    with only a qr-code as content.

--- modules/core/src/main/scala/docspell/swissqr/QrReader.scala
+++ modules/core/src/main/scala/docspell/swissqr/QrReader.scala
@@ -1,22 +1,36 @@
 package docspell.swissqr
 
-import boofcv.factory.fiducial.FactoryFiducial
+import boofcv.alg.fiducial.qrcode.EciEncoding
+import boofcv.factory.fiducial.{ConfigQrCode, FactoryFiducial}
+import boofcv.factory.filter.binary.{ConfigThreshold, ThresholdType}
+import boofcv.factory.shape.{ConfigPolygonDetector, ConfigPolygonFromContour}
 import boofcv.io.image.ConvertBufferedImage
 import boofcv.struct.image.GrayU8
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject
 import org.apache.pdfbox.pdmodel.{PDDocument, PDPage}
 import org.apache.pdfbox.rendering.{ImageType, PDFRenderer}
 
-import java.awt.RenderingHints
-import java.awt.image.BufferedImage
+import java.awt.{BasicStroke, Color, Graphics2D, Image, RenderingHints}
+import java.awt.image.{BufferedImage, ImageObserver}
+import java.io.File
 import java.util.logging.{Level, Logger}
+import javax.imageio.ImageIO
 import scala.jdk.CollectionConverters.*
 
 object QrReader:
   private[this] val logger: Logger = Logger.getLogger("QrReader")
 
+  private val maxImageSize = 600 * 600
+
   def readImage(img: BufferedImage): List[Either[String, SwissQR]] =
-    val detector = FactoryFiducial.qrcode(null, classOf[GrayU8])
+    val qrCodes = readImage1(img)
+    if (qrCodes.isEmpty && img.getHeight * img.getWidth < maxImageSize) {
+      readImage1(createBorderImage(img))
+    } else qrCodes
+
+  private def readImage1(img: BufferedImage): List[Either[String, SwissQR]] =
+    val cfg = ConfigQrCode()
+    val detector = FactoryFiducial.qrcode(cfg, classOf[GrayU8])
     detector.process(ConvertBufferedImage.convertFrom(img, null: GrayU8))
     detector.getDetections.asScala.map(qr => QrTextReader.read(qr.message)).toList
 
@@ -24,10 +38,32 @@ object QrReader:
     val renderer = createPdfRenderer(doc)
     (0 until doc.getNumberOfPages).flatMap { index =>
       val page = doc.getPage(index)
-      renderer.setSubsamplingAllowed(enableSubsampling(doc.getPage(index)))
+      renderer.setSubsamplingAllowed(enableSubsampling(page))
       val img = renderer.renderImageWithDPI(index, dpi, ImageType.RGB)
       readImage(img)
     }
+
+  /** Draws a white border around an image to help with qr code detection. */
+  private def createBorderImage(img: BufferedImage): BufferedImage =
+    val borderWidth = 1
+    val out = new BufferedImage(
+      img.getWidth + (borderWidth * 2),
+      img.getHeight + (borderWidth * 2),
+      BufferedImage.TYPE_USHORT_GRAY
+    )
+    val g = out.getGraphics.asInstanceOf[Graphics2D]
+    g.setBackground(Color.WHITE)
+    g.setPaint(Color.WHITE)
+    g.setStroke(new BasicStroke(borderWidth * 2.0f))
+    g.drawRect(0, 0, out.getWidth, out.getHeight)
+    g.drawImage(
+      img,
+      borderWidth,
+      borderWidth,
+      Color.WHITE,
+      null
+    )
+    out
 
   private def createPdfRenderer(doc: PDDocument) =
     val renderer = new PDFRenderer(doc)
@@ -43,6 +79,10 @@ object QrReader:
     hints.put(
       RenderingHints.KEY_INTERPOLATION,
       RenderingHints.VALUE_INTERPOLATION_BICUBIC
+    )
+    hints.put(
+      RenderingHints.KEY_ALPHA_INTERPOLATION,
+      RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY
     )
     hints.put(
       RenderingHints.KEY_TEXT_ANTIALIASING,
@@ -61,7 +101,7 @@ object QrReader:
         .map { imgobj =>
           val w = imgobj.getWidth
           val h = imgobj.getHeight
-          logger.log(Level.FINE, s"Found image in pdf of size ${w}x${h} (${w * h}px)")
+          logger.log(Level.FINE, s"Found image in pdf of size ${w}x$h (${w * h}px)")
           w * h
         }
         .maxOption
